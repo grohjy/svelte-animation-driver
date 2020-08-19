ebe7a7b50ccf0c5bbb9bc1413d14caa287e07b66
unknown <jyrki.grohn@gmail.com>
2020-08-19 22:28:53 +0300

    helper functions added

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,93 @@
 <script>
   import { bounceIn } from 'svelte/easing'
   import { animationDriver } from './animationDriver.js'
+
   let name = 'world'
   const anim = animationDriver()
-  let x1, x2
-  let mouseXStart
-  let mouseX
-  let playheadPos
-  let isDrag = false
-  let wasPlaying = false
-  anim.addAnimation(1000, 2000, (t) => (x1 = t))
-  anim.addAnimation(0, 6000, (t) => (x2 = t))
+  let props = anim.propsStore
+
+  let x1, x2, yWidth
+  let playheadPosition
+  let ko = (t, u) => {
+    x1 = t
+    yWidth = divYellow.repeat(2).cycle().valueOf() * 70
+  }
+  let divYellow = new anim.addAnimation(0, 8000, ko)
+  let divGray = new anim.addAnimation(1000, 6000, (t, u) => (x2 = t))
+  new anim.addAnimation(0, 6000, (t, u) => (playheadPosition = t))
   anim.setPlayhead(0)
-  const down = (e) => {
-    isDrag = true
-    mouseXStart = e.clientX
-    playheadPos = anim.getPlayhead()
-    wasPlaying = anim.animPlaying()
-    anim.pause()
-  }
-  const move = (e) => {
-    if (isDrag) {
-      mouseX = e.clientX - mouseXStart
-      let w = e.target.clientWidth
-      let frac = (mouseX / w) * 2
-      anim.setPlayhead(playheadPos + frac)
-    }
-  }
-  const up = (e) => {
-    if (isDrag) {
-      if (wasPlaying) anim.start()
-      isDrag = false
-    }
-  }
 </script>
 
-<!-- <h1 on:click={() => anim.setPlayhead(2)}>0.1</h1> -->
-<span on:click={() => anim.rewind()}>Rewind</span>
-<span on:click={() => anim.pause()}>Pause</span>
-<span on:click={() => anim.play()}>Play</span>
-<br />
-<span on:click={() => anim.setSpeed(0.5)}>x0.5</span>
-<span on:click={() => anim.setSpeed(1)}>x1</span>
-<span on:click={() => anim.setSpeed(4)}>x4</span>
-<p>Drag mouse to run animation</p>
+<style>
+  .b {
+    width: 30px;
+    padding: 3px;
+    border: 1px solid black;
+    text-align: center;
+  }
+  .b.sel {
+    background-color: lightgray;
+  }
+</style>
+
 <div
-  on:mousedown={down}
-  on:mousemove={move}
-  on:mouseup={up}
-  on:mouseout={up}
-  style="background-color:pink;width:300px;height:50px;">
-  &nbsp
+  style="display: flex;width:150px;height:
+  40px;justify-content:center;align-items: center;">
+  {#if $props.playing && $props.direction == 'rew'}
+    <div on:click={() => anim.pause()}>⏸</div>
+  {:else}
+    <div on:click={() => anim.rewind()}>◀️</div>
+  {/if}
+  <div on:click={() => anim.reset()}>⏹</div>
+  {#if $props.playing && $props.direction == 'fwd'}
+    <div on:click={() => anim.pause()}>⏸</div>
+  {:else}
+    <div on:click={() => anim.play()}>▶️</div>
+  {/if}
 </div>
-<div style="position:relative">
-  <p
-    style="background-color:yellow;width:100px;transform: translate({x1 * 100}px,
-    0)">
+<div
+  style="display: flex;width: 200px;height: 40px;justify-content:
+  space-around;align-items: center;">
+  Speed:
+  <div
+    class="b"
+    class:sel={$props.speed == 0.5}
+    on:click={() => ($props.speed = 0.5)}>
+    x0.5
+  </div>
+  <div
+    class="b"
+    class:sel={$props.speed == 1}
+    on:click={() => ($props.speed = 1)}>
     x1
-  </p>
-  <p
-    style="background-color:gray;width:100px;transform: translate({x2 * 150}px, {bounceIn(1 - x2) * -100}px)">
+  </div>
+  <div
+    class="b"
+    class:sel={$props.speed == 4}
+    on:click={() => ($props.speed = 4)}>
+    x4
+  </div>
+</div>
+<p>
+  <span>Playhead:</span>
+  <br />
+  <input
+    on:input={(e) => anim.setPlayhead(e.target.value / 1000)}
+    type="range"
+    min="0"
+    max="1000"
+    value={playheadPosition * 1000} />
+</p>
+
+<div style="position:relative; height:60px;border:1px solid;padding:2px;padding-top:70px">
+  <div
+    style="background-color:yellow;width:{30 + yWidth}px;height:30px;transform:
+    translate({x1 * 300}px, 0)">
+    x1
+  </div>
+  <div
+    style="border:1px solid red;width:100px;height:30px;transform: translate({x2 * 300}px,
+    {bounceIn(1 - x2) * -100}px)">
     x2
-  </p>
+  </div>
 </div>
--- src/animationDriver.js
+++ src/animationDriver.js
@@ -1,4 +1,5 @@
 import { tweened } from "svelte/motion";
+import { writable } from "svelte/store";
 
 export function animationDriver() {
   const store = tweened(0);
@@ -11,52 +12,12 @@ export function animationDriver() {
   let isPlaying = false;
   let playingDir = "fwd";
 
-  const updateStatus = (tvalue) => {
-    playhead = tvalue;
-    if (playhead >= 1) isPlaying = false;
-    if (playhead <= 0) isPlaying = false;
-    cbSubAnimations.forEach((subanim) => {
-      let subAnimT = getAnimationT(subanim.delay, subanim.dur);
-      subanim.fn(subAnimT);
-    });
-  };
-
-  store.subscribe((v) => {
-    updateStatus(v);
+  const propsStore = writable({
+    totalDuration: duration,
+    speed: currentSpeed,
+    playing: isPlaying,
+    direction: playingDir,
   });
-
-  const addAnimation = (delay, dur, fn) => {
-    let animdur = delay + dur;
-    duration = duration < animdur ? animdur : duration;
-    setSpeed(currentSpeed);
-    cbSubAnimations.push({ delay: delay, dur: dur, fn: fn });
-  };
-
-  const getAnimationT = (delay, dur) => {
-    let t;
-    delay = delay / currentSpeed;
-    dur = dur / currentSpeed;
-    let driverDur = speedAdjDuration;
-    if (delay > 0)
-      t =
-        (((playhead - delay / driverDur) / ((driverDur - delay) / driverDur)) *
-          (driverDur - delay)) /
-        dur;
-    else t = (playhead * driverDur) / dur;
-    if (t <= 0) return 0;
-    if (t > 1) return 1;
-    return t;
-  };
-
-  const run = () => {
-    if (playingDir == "fwd") {
-      store.set(1, { duration: speedAdjDuration * (1 - playhead) });
-      isPlaying = true;
-    } else if (playingDir == "rew") {
-      store.set(0, { duration: speedAdjDuration * playhead });
-      isPlaying = true;
-    }
-  };
 
   const tryNumber = (nb) => {
     try {
@@ -79,11 +40,135 @@ export function animationDriver() {
     tryGtZero(speed);
     currentSpeed = speed;
     speedAdjDuration = duration / currentSpeed;
-    if (isPlaying) run();
   };
 
-  const getSpeed = () => {
-    return currentSpeed;
+  const run = () => {
+    if (isPlaying) {
+      if (playingDir == "fwd" && playhead < 1) {
+        store.set(1, { duration: speedAdjDuration * (1 - playhead) });
+      } else if (playingDir == "rew" && playhead > 0) {
+        store.set(0, { duration: speedAdjDuration * playhead });
+      }
+    }
+  };
+
+  propsStore.subscribe((value) => {
+    run();
+  });
+  propsStore.set = (value) => {
+    if (typeof value !== "object") value = {};
+    if ("speed" in value && typeof value.speed === "number")
+      setSpeed(value.speed);
+    else delete value.speed;
+    if ("playing" in value && typeof value.playing === "boolean")
+      isPlaying = value.playing;
+    else delete value.playing;
+    if ("direction" in value && typeof value.direction === "string")
+      playingDir = value.direction;
+    else delete value.direction;
+    propsStore.update((oldValue) => {
+      return { ...oldValue, ...value };
+    });
+  };
+
+  const updateStatus = (tvalue) => {
+    playhead = tvalue;
+    if (playhead >= 1) {
+      propsStore.set({ playing: false });
+    }
+    if (playhead <= 0) {
+      propsStore.set({ playing: false });
+    }
+    cbSubAnimations.forEach((subanim) => {
+      // let { t, u } = getAnimationT(subanim.delay, subanim.dur);
+      // subanim.fn(t, u);
+      subanim();
+    });
+  };
+
+  store.subscribe((v) => {
+    updateStatus(v);
+  });
+
+  function addAnimation(delay, dur, fn) {
+    let animdur = delay + dur;
+    duration = duration < animdur ? animdur : duration;
+    propsStore.set({ speed: currentSpeed });
+    let tAnim;
+    const upd = () => {
+      let t = getAnimationT(delay, dur);
+      tAnim = t;
+      fn(t, 1 - t);
+    };
+    cbSubAnimations.push(upd);
+    // cbSubAnimations.push({ delay: delay, dur: dur, fn: fn });
+
+    this.keyframes = function (startFrame, endFrame) {
+      if (startFrame > 1) {
+        startFrame = startFrame / 100;
+        endFrame = endFrame / 100;
+      }
+      let tModified = 0;
+      if (endFrame < startFrame) {
+        let temp = endFrame;
+        endFrame = startFrame;
+        startFrame = temp;
+      }
+      if (tAnim < startFrame) tModified = 0;
+      else if (tAnim > endFrame) tModified = 1;
+      else {
+        tModified = (tAnim - startFrame) / (endFrame - startFrame);
+      }
+      tAnim = tModified;
+      return this;
+    };
+    this.repeat = function (repeat) {
+      if (repeat < 1) repeat = 1;
+      let tModified = 0;
+      tModified = (tAnim % (1 / repeat)) * repeat;
+      tAnim = tModified;
+      return this;
+    };
+    this.steps = function (steps) {
+      if (steps < 1) steps = 1;
+      let tModified = 0;
+      tModified = Math.round(tAnim * steps) / steps;
+      tAnim = tModified;
+      return this;
+    };
+    this.cycle = function () {
+      let tModified = 0;
+      if (tAnim <= 0.5) tModified = tAnim;
+      else tModified = 1 - tAnim;
+      tAnim = tModified;
+      return this;
+    };
+    this.valueOf = function () {
+      return tAnim;
+    };
+  }
+
+  const addAnimation2 = (delay, dur, fn) => {
+    let animdur = delay + dur;
+    duration = duration < animdur ? animdur : duration;
+    propsStore.set({ speed: currentSpeed });
+    cbSubAnimations.push({ delay: delay, dur: dur, fn: fn });
+  };
+
+  const getAnimationT = (delay, dur) => {
+    let t;
+    delay = delay / currentSpeed;
+    dur = dur / currentSpeed;
+    let driverDur = speedAdjDuration;
+    if (delay > 0)
+      t =
+        (((playhead - delay / driverDur) / ((driverDur - delay) / driverDur)) *
+          (driverDur - delay)) /
+        dur;
+    else t = (playhead * driverDur) / dur;
+    if (t <= 0) t = 0;
+    if (t > 1) t = 1;
+    return t;
   };
 
   const setPlayhead = (value) => {
@@ -91,7 +176,6 @@ export function animationDriver() {
     if (value > 1) value = 1;
     if (value < 0) value = 0;
     if (playhead == value) {
-      console.log('sama')
       store.set(value + 0.0001, { duration: 0 });
     }
     store.set(value, { duration: 0 });
@@ -103,37 +187,47 @@ export function animationDriver() {
   };
 
   const pause = () => {
-    isPlaying = false;
+    propsStore.set({
+      playing: false,
+    });
     setPlayhead(playhead);
   };
 
   const play = () => {
-    playingDir = "fwd";
-    run();
+    propsStore.set({
+      playing: true,
+      direction: "fwd",
+    });
   };
 
   const rewind = () => {
-    playingDir = "rew";
-    run();
+    propsStore.set({
+      playing: true,
+      direction: "rew",
+    });
+  };
+
+  const reset = () => {
+    propsStore.set({
+      playing: false,
+      direction: "fwd",
+    });
+    setPlayhead(0);
   };
 
   const start = () => {
-    run();
-  };
-  const animPlaying = () => {
-    return isPlaying;
+    propsStore.set({ playing: true });
   };
 
   return {
+    propsStore,
     addAnimation,
-    setSpeed,
-    getSpeed,
     setPlayhead,
     getPlayhead,
     pause,
     start,
     play,
     rewind,
-    animPlaying,
+    reset,
   };
 }
